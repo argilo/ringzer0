14cad79c18f9ac8f6e8b3c221d6d29bc76d0cbb3
Clayton Smith <argilo@gmail.com>
2020-11-23 19:39:11 -0500

    Add solution for 34

---
+++ 34/script.js
@@ -0,0 +1,28 @@
+<script>
+  // Look's like weak JavaScript auth script :)
+  $(".c_submit").click(function(event) {
+    event.preventDefault();
+    var k = new Array(176,214,205,246,264,255,227,237,242,244,265,270,283);
+    var u = $("#cuser").val();
+    var p = $("#cpass").val();
+    var t = true;
+
+    if(u == "administrator") {
+      for(i = 0; i < u.length; i++) {
+        if((u.charCodeAt(i) + p.charCodeAt(i) + i * 10) != k[i]) {
+          $("#cresponse").html("<div class='alert alert-danger'>Wrong password sorry.</div>");
+          t = false;
+          break;
+        }
+      }
+    } else {
+      $("#cresponse").html("<div class='alert alert-danger'>Wrong password sorry.</div>");
+      t = false;
+    }
+    if(t) {
+      if(document.location.href.indexOf("?p=") == -1) {
+        document.location = document.location.href + "?p=" + p;
+                }
+    }
+  });
+</script>
---
+++ 34/solution.js
@@ -0,0 +1,7 @@
+var k = new Array(176,214,205,246,264,255,227,237,242,244,265,270,283);
+var u = "administrator";
+var p = "";
+for (i = 0; i < u.length; i++) {
+  p += String.fromCharCode(k[i] - u.charCodeAt(i) - i * 10);
+}
+p;
